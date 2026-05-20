46d2b74fc503e3d83a4222b1aa56e9dd9e949a52
Silvia Damiani <damianisilvia1@gmail.com>
2026-05-20 13:32:55 +0200

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './components/HomePage'
 import DashboardPage from './components/DashboardPage'
+import Layout from './components/Layout'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   )
 }
